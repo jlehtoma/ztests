2eaa64fcdf699ccdf5bba5d5f2c0b7b353b080b4
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-05-09 15:18:53 +0300

    Added hardware info document

---
+++ hardware_info.md
@@ -0,0 +1,8 @@
+## BIOTI25
+
+Processor: Intel(R) Core(TM) i7-3667U CPU @ 2.00GHz, 2501 Mhz, 2 Core(s), 4 Logical Processor(s)
+
+RAM: 8.0 GB
+
+## MRGTESLA
+
--- run_esmk_test_suite.bat
+++ run_esmk_test_suite.bat
@@ -1,2 +1,2 @@
-python zbenchmark.py -l test_suite_esmk_win_MRGTESLA.yaml -o results_esmk_win_MRGTESLA_x86_64.yaml
+python zbenchmark.py -l test_suite_esmk_win_MRGSITE25.yaml -o results_esmk_win_MRGSITE25_x86_64.yaml
 
