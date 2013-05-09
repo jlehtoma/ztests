691f5577c6572ee7b79518ed94c60cea29729b08
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-05-09 14:07:09 +0300

    Plotting results

--- plot_results.R
+++ plot_results.R
@@ -2,37 +2,72 @@ library(ggplot2)
 library(reshape2)
 library(yaml)
 
-setwd("/home/jlehtoma/opt/zonation-3.1.9-GNU-Linux/ztests/")
 
-dat.linux <- yaml.load_file("results_linux_x86_64.yaml")
-dat.win <- yaml.load_file("results_win7_x86_64.yaml")
+# 1. Setup ----------------------------------------------------------------
+
+setwd("C:/Users/localadmin_jlehtoma/Documents/GitHub/ztests")
+
+dat.linux <- yaml.load_file("results_esmk_linux_x86_64.yaml")
+dat.win <- yaml.load_file("results_esmk_win7_x86_64.yaml")
+
+
+# 2. Read in the data -----------------------------------------------------
 
 # Get all keys
 runs.linux <- names(dat.linux)
 runs.win <- names(dat.win)
 # Keep only the bat-paths
-runs.linux <- runs.linux[grepl("^/home.*\\.bat", runs.linux)]
+runs.linux <- runs.linux[grepl("^/home.*\\.sh", runs.linux)]
 runs.win <- runs.win[grepl("*\\.bat", runs.win)]
 
-times.linux <- c()
-times.win <- c()
-
-for (run.linux in runs.linux) {
-  run.name.linux <- basename(run.linux)
-  times.linux <- c(times.linux, unlist(dat.linux[run.linux]))
-  run.win <- runs.win[grepl(run.name.linux, runs.win)]
-  run.name.win <- basename(run.win)
-  times.win <- c(times.win, unlist(dat.win[run.win]))
+get.times <- function(file.name, data) {
+  run.name <- basename(file.name)
+  run.name <- gsub(".sh", "", run.name)
+  run.name <- gsub(".bat", "", run.name)
+  
+  return(c(run.name, unlist(data[file.name], use.names=FALSE)))
 }
 
-time.data <- data.frame(run=basename(runs.linux), linux=times.linux, 
-                        win=times.win)
-# Get rid of do_load.bat, for some reason didn't run well on Win7...
-time.data <- time.data[-which(time.data$run == "do_load.bat"),]
-# Get rid of do_comm_original_rep.bat, something fishy in Linux...
-time.data <- time.data[-which(time.data$run == "do_comm_original_rep.bat"),]
+col.as.numeric <- function(x) {
+  return(as.numeric(as.character(x)) )
+}
+
+header <- c("run", "cellrem", "elapsed", "init", "measured")
+
+df.linux <- data.frame(do.call("rbind", lapply(runs.linux, get.times, 
+                                               dat.linux)))
+colnames(df.linux) <- header
+df.linux$cellrem <- col.as.numeric(df.linux$cellrem) 
+df.linux$elapsed <- col.as.numeric(df.linux$elapsed)
+df.linux$init <- col.as.numeric(df.linux$init)
+df.linux$measured <- col.as.numeric(df.linux$measured)
+
+#df.win <- data.frame(do.call("rbind", lapply(runs.win, get.times, dat.win)))
+#colnames(df.win) <- header
+
+
+# 3. Stats ----------------------------------------------------------------
 
 (mean(time.data$linux / time.data$win))
+
+
+# 4. Plotting -------------------------------------------------------------
+
+
+# 4.1 Total elapsed time --------------------------------------------------
+
+# Linux
+p <- ggplot(df.linux, aes(y=elapsed, x=run)) + 
+     geom_bar(position="dodge", stat="identity")
+p + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + ylab("Seconds") 
+
+p <- ggplot(df.linux, aes(y=init, x=run)) + 
+  geom_bar(position="dodge", stat="identity")
+p + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + ylab("Seconds")
+
+# 4.2 Per stage -----------------------------------------------------------
+
+
 
 m.time.data <- melt(time.data, variable.name="OS")
 
---
+++ run_esmk_test_suite.bat
@@ -0,0 +1,1 @@
+./zbenchmark.py -l test_suite_esmk_win.yaml -o results_esmk_win_MRGSITE25_x86_64.yaml
